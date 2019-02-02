ab7f8186316953ef6355c8b60aef1dd2c3a40199
WosLovesLife <zhang00h00@126.com>
2019-02-02 16:09:00 +0800

    build flip_view and make a simple example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,56 +1,172 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_flip_view/flutter_flip_view.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: SimpleFlip()
+    ));
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+class SimpleFlip extends StatefulWidget {
+    @override
+    _SimpleFlipState createState() => _SimpleFlipState();
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class _SimpleFlipState extends State<SimpleFlip> with SingleTickerProviderStateMixin {
+    AnimationController _animationController;
+    Animation<double> _curvedAnimation;
 
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
+    GlobalKey _frontKey = GlobalKey();
+    Size _backSize = Size.zero;
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterFlipView.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+    @override
+    void initState() {
+        super.initState();
+
+        _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 1000));
+        _curvedAnimation = CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
+
+        WidgetsBinding.instance.addPostFrameCallback(_afterFrame);
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+    _afterFrame(_) {
+        final reverseSize = _frontKey.currentContext?.size;
+        if (reverseSize == null || reverseSize.isEmpty) {
+            WidgetsBinding.instance.addPostFrameCallback(_afterFrame);
+        } else {
+            setState(() {
+                _backSize = reverseSize;
+            });
+        }
+    }
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+    @override
+    void dispose() {
+        _animationController.dispose();
+        super.dispose();
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
-    );
-  }
+    void _flip(bool reverse) {
+        if (_animationController.isAnimating) return;
+        if (reverse) {
+            _animationController.forward();
+        } else {
+            _animationController.reverse();
+        }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            home: Scaffold(
+                body: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Center(
+                        child: FlipView(
+                            animationController: _curvedAnimation,
+                            front: _buildFrontSide(),
+                            back: _buildBackSide(),
+                        ),
+                    ),
+                ),
+            ),
+        );
+    }
+
+    Widget _buildFrontSide() {
+        return AspectRatio(
+            key: _frontKey,
+            aspectRatio: 0.7,
+            child: Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        gradient: LinearGradient(
+                            colors: [
+                                Color(0xff92ffc0),
+                                Color(0Xff002661),
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                        ),
+                    ),
+                    child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                            splashColor: Colors.white.withOpacity(0.1),
+                            highlightColor: Colors.transparent,
+                            borderRadius: BorderRadius.circular(8),
+                            onTap: () => _flip(true),
+                            child: Center(
+                                child: Container(
+                                    width: 48,
+                                    height: 48,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(60),
+                                        color: Colors.amber,
+                                    ),
+                                    child: Text(
+                                        'A',
+                                        style:
+                                        TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        );
+    }
+
+    Widget _buildBackSide() {
+        print(_backSize);
+        return SizedBox(
+            width: _backSize.width,
+            height: _backSize.height,
+            child: Card(
+                color: Colors.blueGrey,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        gradient: LinearGradient(
+                            colors: [
+                                Color(0xff92ffc0),
+                                Color(0Xff002661),
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                        ),
+                    ),
+                    child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                            splashColor: Colors.white.withOpacity(0.1),
+                            highlightColor: Colors.transparent,
+                            borderRadius: BorderRadius.circular(8),
+                            onTap: () => _flip(false),
+                            child: Center(
+                                child: Container(
+                                    width: 48,
+                                    height: 48,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(60),
+                                        color: Colors.amber,
+                                    ),
+                                    child: Text(
+                                        'B',
+                                        style:
+                                        TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        );
+    }
 }
--- ios/Classes/FlutterFlipViewPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterFlipViewPlugin : NSObject<FlutterPlugin>
-@end
